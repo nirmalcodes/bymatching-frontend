591d8d236b5078d8d83e4d2b14beba525adf09e0
nirmalcodes <119014888+nirmalcodes@users.noreply.github.com>
2023-02-22 09:47:39 +0530

    Update components js

---
+++ src/components/index.js
@@ -0,0 +1,22 @@
+export { default as Navbar } from "./Navbar/Navbar";
+export { default as Navbar1 } from "./Navbar/Navbar1";
+
+export { default as Footer } from "./Footer/Footer";
+export { default as Footer1 } from "./Footer/Footer1";
+
+export { default as FooterLinks } from "./FooterLinks/FooterLinks";
+export { default as FooterLinks1 } from "./FooterLinks/FooterLinks1";
+
+export { default as CircleItem } from "./CircleItem/CircleItem";
+
+export { default as Divider } from "./Divider/Divider";
+
+export { default as SocialMediaBtn } from "./SocialMediaBtn/SocialMediaBtn";
+export { default as SocialMediaBtn1 } from "./SocialMediaBtn/SocialMediaBtn1";
+
+export { default as BlogPostCard } from "./BlogPostCard/BlogPostCard";
+export { default as BlogPostCard1 } from "./BlogPostCard/BlogPostCard1";
+
+export { default as SectionWrapper } from "./SectionWrapper/SectionWrapper";
+
+export { default as ImageContentRow } from "./ImageContentRow/ImageContentRow";
