584717bf40c4c72e9b3c2859634886199cf2f519
nirmalcodes <119014888+nirmalcodes@users.noreply.github.com>
2023-03-07 09:54:51 +0530

    Add page not found

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -1,7 +1,10 @@
 import React from "react";
 import { images } from "../../constants";
+import { useNavigate } from "react-router-dom";
 
 const NotFound = () => {
+  const navigate = useNavigate();
+
   return (
     <>
       <div
@@ -10,9 +13,16 @@ const NotFound = () => {
           backgroundImage: `url(${images.background_shapes})`,
         }}
       >
-        <div className="text-center text-6xl text-white xl:text-9xl">
-          <p className="text-center font-bold text-8xl xl:text-9xl">404</p>
-          Page Not Found
+        <div className="text-center text-white">
+          <p className="text-center text-8xl font-bold xl:text-9xl">404</p>
+          <p className="text-6xl font-medium xl:text-9xl">Page Not Found</p>
+          <button
+            type="button"
+            onClick={() => navigate(-1)}
+            className="my-16 rounded-full border-2 border-white bg-white/10 py-3 px-12 text-lg font-medium text-white backdrop-blur-2xl transition-all duration-300"
+          >
+            Go back to previous page
+          </button>
         </div>
       </div>
     </>
